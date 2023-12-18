4dd5738a49dfdf2596927324433de3f131bc80b9
Nick <darkforceyoda@gmail.com>
2023-12-18 09:52:39 -0600

    user can create post

--- app/api/create-post/route.ts
+++ app/api/create-post/route.ts
@@ -1,7 +1,17 @@
 import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma/prisma";
 
 export async function POST(request: any) {
   const res = await request.json();
+  const { title, content } = res;
   console.log({ res });
-  return NextResponse.json({ data: res });
+
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      author: { create: { name: "Nick" } }, //fix later
+    },
+  });
+  return NextResponse.json({ result });
 }

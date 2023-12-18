774efcd9bd586b9156ae916a1bd12c5fef91af91
Nick <darkforceyoda@gmail.com>
2023-12-17 18:00:20 -0600

    prisma schema

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,12 +1,11 @@
-import Card from "@/app/components/shared/Card";
+import Card from "@/components/shared/Card";
 import React from "react";
 
 type pageProps = {};
 
 const page: React.FC<pageProps> = () => {
   return (
-    <section className="w-full border border-red-500 text-gray-200">
-    </section>
+    <section className="w-full border border-red-500 text-gray-200"></section>
   );
 };
 export default page;

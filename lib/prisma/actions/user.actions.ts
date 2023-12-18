06be71c562e70ac7982357beddbfd86f61d921db
Nick <darkforceyoda@gmail.com>
2023-12-17 19:26:13 -0600

    fetchProfiles user action

---
+++ lib/prisma/actions/user.actions.ts
@@ -0,0 +1,14 @@
+"use server";
+import prisma from "../prisma";
+
+export async function fetchPosts() {
+  const posts = await prisma.post.findMany({
+    // where: { published: true },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  });
+  return posts;
+}

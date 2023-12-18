af5cd1386255f71f2c2001d13793397a751a5b3b
Nick <darkforceyoda@gmail.com>
2023-12-18 09:53:10 -0600

    refresh page on Post

--- components/forms/PostForm.tsx
+++ components/forms/PostForm.tsx
@@ -1,22 +1,25 @@
 "use client";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 type PostFormProps = {};
 
 const PostForm: React.FC<PostFormProps> = () => {
+  const router = useRouter();
   const [title, setTitle] = useState("title test");
   const [content, setContent] = useState("content test");
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
     try {
-      fetch("/api/create-post", {
+      await fetch("/api/create-post", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ title, content }),
       });
+      router.refresh();
     } catch (error) {
       console.log("Create Post Error: ", error);
     }

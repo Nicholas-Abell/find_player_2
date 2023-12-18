75cc3b12918098533a31196fbdbd1341ecaafabd
Nick <darkforceyoda@gmail.com>
2023-12-17 23:06:58 -0600

    db roles

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,13 @@
+import { fetchPosts } from "@/lib/prisma/actions/user.actions";
 import Card from "../../components/shared/Card";
 
-export default function Home() {
+export default async function Home() {
+  let posts = await fetchPosts();
   return (
     <section className="w-full min-h-screen text-gray-200 grid md:grid-cols-2 xl:grid-cols-3 grid-rows-3 r gap-4 p-4">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {posts.map((post, key) => (
+        <Card gameTitle={post.title} content={post.content} key={key} />
+      ))}
     </section>
   );
 }
--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,18 +1,35 @@
 import React from "react";
+import { FaShield } from "react-icons/fa6";
+import { GiHealthNormal, GiHeavyBullets } from "react-icons/gi";
 
-type CardProps = {};
+type CardProps = {
+  gameTitle: string;
+  content?: string | null;
+};
 
-const Card: React.FC<CardProps> = () => {
+const Card: React.FC<CardProps> = ({ gameTitle, content }) => {
   return (
     <div className="flex flex-col justify-between items-center py-4 bg-gray-800 px-4 gap-4">
-      <h3>Game</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-        quidem corrupti consequuntur vero iusto saepe error sunt culpa.
-        Provident reiciendis doloribus, cupiditate impedit facilis animi!
-        Corporis quas voluptatem excepturi maxime?
-      </p>
-      <p>asdf asdf asdf asdf asdf</p>
+      <h3>{gameTitle}</h3>
+      <p>{content}</p>
+
+      <div className="grid grid-cols-3 gap-4">
+        <div className="flex flex-col justify-between items-center gap-4">
+          <p>Tank</p>
+          <FaShield size={25} />
+          <p>check</p>
+        </div>
+        <div className="flex flex-col justify-between items-center gap-4">
+          <p>Damage</p>
+          <GiHeavyBullets size={25} />
+          <p>check</p>
+        </div>
+        <div className="flex flex-col justify-between items-center gap-4">
+          <p>Healer</p>
+          <GiHealthNormal size={25} />
+          <p>check</p>
+        </div>
+      </div>
     </div>
   );
 };

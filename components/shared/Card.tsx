6aeddaeed0e2b630cc375f7d298da2e4931074ac
Nick <darkforceyoda@gmail.com>
2023-12-18 12:47:01 -0600

    roles prop, style based on roles

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -5,32 +5,44 @@ import { FaShield } from "react-icons/fa6";
 import { GiHealthNormal, GiHeavyBullets } from "react-icons/gi";
 
 type CardProps = {
-  gameTitle: string;
+  title: string;
   content?: string | null;
+  roles?: string | null;
   id: string;
 };
 
-const Card: React.FC<CardProps> = ({ gameTitle, content, id }) => {
+const Card: React.FC<CardProps> = ({ title, content, roles, id }) => {
   return (
     <div className="flex flex-col justify-between items-center py-4 bg-gray-800 px-4 gap-4">
-      <h3>{gameTitle}</h3>
+      <h3>{title}</h3>
       <p>{content}</p>
-      <button className="bg-red-600 text-white" onClick={() => DeletePost(id)}>
+      {/* <button className="bg-red-600 text-white" onClick={() => DeletePost(id)}>
         Delete
-      </button>
+      </button> */}
 
       <div className="grid grid-cols-3 gap-4">
-        <div className="flex flex-col justify-between items-center gap-4">
+        <div
+          className={`${
+            roles?.includes("tank") || null ? "text-white" : "text-gray-600"
+          } flex flex-col justify-between items-center gap-4 py-4`}
+        >
           <p>Tank</p>
           <FaShield size={25} />
           <p>check</p>
         </div>
-        <div className="flex flex-col justify-between items-center gap-4">
+        <div
+          className={`${
+            roles?.includes("damage") || null ? "text-white" : "text-gray-600"
+          } flex flex-col justify-between items-center gap-4 py-4`}
+        >
           <p>Damage</p>
           <GiHeavyBullets size={25} />
-          <p>check</p>
         </div>
-        <div className="flex flex-col justify-between items-center gap-4">
+        <div
+          className={`${
+            roles?.includes("healer") || null ? "text-white" : "text-gray-600"
+          } flex flex-col justify-between items-center gap-4 py-4`}
+        >
           <p>Healer</p>
           <GiHealthNormal size={25} />
           <p>check</p>

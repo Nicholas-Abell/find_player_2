abe8641373408dbf1224a48c1821b2a067abe478
Nick <darkforceyoda@gmail.com>
2023-12-22 11:24:29 -0600

    commented traditional routes

--- app/api/create-post/route.ts
+++ app/api/create-post/route.ts
@@ -1,20 +1,20 @@
-import { NextResponse } from "next/server";
-import prisma from "@/lib/prisma/prisma";
+// import { NextResponse } from "next/server";
+// import prisma from "@/lib/prisma/prisma";
 
-export async function POST(request: any) {
-  const res = await request.json();
-  const { title, content } = res;
-  console.log({ res });
+// export async function POST(request: any) {
+//   const res = await request.json();
+//   const { title, content } = res;
+//   console.log({ res });
 
-  const result = await prisma.post.create({
-    data: {
-      title,
-      content,
-      author: { create: { name: "Nick" } }, //fix later
-    },
-  });
-  return NextResponse.json({ result });
-}
+//   const result = await prisma.post.create({
+//     data: {
+//       title,
+//       content,
+//       author: { create: { name: "Nick" } }, //fix later
+//     },
+//   });
+//   return NextResponse.json({ result });
+// }
 
    // try {
     //   await fetch("/api/create-post", {

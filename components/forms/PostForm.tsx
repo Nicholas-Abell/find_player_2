30036b2d95c2900e88cc605c297926b11d2e8905
Nick Abell <darkforceyoda@gmail.com>
2023-12-29 10:10:36 -0800

    style: Post Form

--- components/forms/PostForm.tsx
+++ components/forms/PostForm.tsx
@@ -21,7 +21,7 @@ const PostForm: React.FC<PostFormProps> = ({ id }) => {
     title: "",
     content: "",
     roles: [],
-    id
+    id,
   });
 
   const handleSubmit = async (e: any) => {
@@ -36,83 +36,95 @@ const PostForm: React.FC<PostFormProps> = ({ id }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        <p>Title</p>
-        <input
-          type="text"
-          onChange={(e) => {
-            setFormData((prevData) => ({
-              ...prevData,
-              title: e.target.value,
-            }));
-          }}
-        />
-      </label>
-      <label>
-        <p>Content</p>
-        <input
-          type="text"
-          onChange={(e) => {
-            setFormData((prevData) => ({
-              ...prevData,
-              content: e.target.value,
-            }));
-          }}
-        />
-      </label>
-      <div>
-        <label>Roles:</label>
+    <div className="w-full px-4 border border-gray-700 py-2">
+      <form
+        onSubmit={handleSubmit}
+        className="mx-auto flex flex-col justify-center gap-2"
+      >
+        <label>
+          <p>Title</p>
+          <input
+            type="text"
+            className="text-gray-200 placeholder:text-gray-400 px-1 w-full bg-gray-700 py-2"
+            placeholder="Genji"
+            onChange={(e) => {
+              setFormData((prevData) => ({
+                ...prevData,
+                title: e.target.value,
+              }));
+            }}
+          />
+        </label>
+        <label>
+          <p>Content</p>
+          <textarea
+            className="text-gray-200 placeholder:text-gray-400 px-1 w-full bg-gray-700 py-2"
+            placeholder="I need healing!"
+            rows={8}
+            onChange={(e) => {
+              setFormData((prevData) => ({
+                ...prevData,
+                content: e.target.value,
+              }));
+            }}
+          />
+        </label>
         <div>
-          <label>
-            <input
-              type="checkbox"
-              name="roles"
-              value="tank"
-              checked={formData.roles.includes("tank")}
-              onChange={() =>
-                setFormData((prevData) => ({
-                  ...prevData,
-                  roles: toggleRole(prevData.roles, "tank"),
-                }))
-              }
-            />
-            Tank
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              name="roles"
-              value="damage"
-              checked={formData.roles.includes("damage")}
-              onChange={() =>
-                setFormData((prevData) => ({
-                  ...prevData,
-                  roles: toggleRole(prevData.roles, "damage"),
-                }))
-              }
-            />
-            Damage
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              name="roles"
-              value="healer"
-              checked={formData.roles.includes("healer")}
-              onChange={() =>
-                setFormData((prevData) => ({
-                  ...prevData,
-                  roles: toggleRole(prevData.roles, "healer"),
-                }))
-              }
-            />
-            Healer
-          </label>
+          <label>Roles:</label>
+          <div className="flex justify-between">
+            <label>
+              <input
+                className="bg-gray-700"
+                type="checkbox"
+                name="roles"
+                value="tank"
+                checked={formData.roles.includes("tank")}
+                onChange={() =>
+                  setFormData((prevData) => ({
+                    ...prevData,
+                    roles: toggleRole(prevData.roles, "tank"),
+                  }))
+                }
+              />
+              Tank
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                name="roles"
+                value="damage"
+                checked={formData.roles.includes("damage")}
+                onChange={() =>
+                  setFormData((prevData) => ({
+                    ...prevData,
+                    roles: toggleRole(prevData.roles, "damage"),
+                  }))
+                }
+              />
+              Damage
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                name="roles"
+                value="healer"
+                checked={formData.roles.includes("healer")}
+                onChange={() =>
+                  setFormData((prevData) => ({
+                    ...prevData,
+                    roles: toggleRole(prevData.roles, "healer"),
+                  }))
+                }
+              />
+              Healer
+            </label>
+          </div>
         </div>
-      </div>
-      <button>Push</button>
-    </form>
+        <button className="bg-blue-800 rounded hover:bg-blue-900 py-2">
+          POST
+        </button>
+      </form>
+    </div>
   );
 };
 export default PostForm;

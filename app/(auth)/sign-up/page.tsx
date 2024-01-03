29fbf998828bb2bdad83dcf3b4ccbcb04a1f0580
Nick Abell <darkforceyoda@gmail.com>
2024-01-03 08:30:39 -0800

    style: sign up form styles to match next auth modal

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -6,7 +6,7 @@ type pageProps = {};
 
 const page: React.FC<pageProps> = () => {
   return (
-    <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="bg-[#161b22] flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         {/* <img
             className="mx-auto h-10 w-auto"
@@ -18,7 +18,7 @@ const page: React.FC<pageProps> = () => {
         </h2>
       </div>
 
-      <div className="bg-slate-800 mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <SignUpForm />
 
         <p className="mt-10 text-center text-sm text-gray-500">
--- components/forms/SignUpForm.tsx
+++ components/forms/SignUpForm.tsx
@@ -4,13 +4,14 @@ type SignUpFormProps = {};
 
 const SignUpForm: React.FC<SignUpFormProps> = () => {
   return (
-    <form className="space-y-6" action="#" method="POST">
+    <form
+      className="space-y-6 bg-[#0d1117] rounded-3xl text-white px-4 py-8"
+      action="#"
+      method="POST"
+    >
       <div>
-        <label
-          htmlFor="email"
-          className="block text-sm font-medium leading-6 text-gray-900"
-        >
-          Email address
+        <label htmlFor="email" className="block text-sm font-medium leading-6">
+          Email address:
         </label>
         <div className="mt-2">
           <input
@@ -19,7 +20,7 @@ const SignUpForm: React.FC<SignUpFormProps> = () => {
             type="email"
             autoComplete="email"
             required
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            className="block bg-[#0d1117] border border-gray-600 rounded-lg w-full rounded-m py-1.5 shadow-sm placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 px-4"
           />
         </div>
       </div>
@@ -28,18 +29,10 @@ const SignUpForm: React.FC<SignUpFormProps> = () => {
         <div className="flex items-center justify-between">
           <label
             htmlFor="password"
-            className="block text-sm font-medium leading-6 text-gray-900"
+            className="block text-sm font-medium leading-6"
           >
-            Password
+            Password:
           </label>
-          <div className="text-sm">
-            <a
-              href="#"
-              className="font-semibold text-indigo-600 hover:text-indigo-500"
-            >
-              Forgot password?
-            </a>
-          </div>
         </div>
         <div className="mt-2">
           <input
@@ -48,7 +41,7 @@ const SignUpForm: React.FC<SignUpFormProps> = () => {
             type="password"
             autoComplete="current-password"
             required
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            className="block bg-[#0d1117] border border-gray-600 rounded-lg w-full rounded-m py-1.5 shadow-sm placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 px-4"
           />
         </div>
       </div>
@@ -56,9 +49,9 @@ const SignUpForm: React.FC<SignUpFormProps> = () => {
       <div>
         <button
           type="submit"
-          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="flex w-full justify-center rounded-md border border-gray-800 hover:bg-slate-800 px-3 py-3 font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
-          Sign in
+          Sign Up
         </button>
       </div>
     </form>

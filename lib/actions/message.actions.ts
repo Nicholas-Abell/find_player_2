3cce17076402a1bdf6f15b3e56c2b011b59e06d7
Nick Abell <darkforceyoda@gmail.com>
2024-01-04 07:49:57 -0800

    CreateMessage server action

--- lib/actions/message.actions.ts
+++ lib/actions/message.actions.ts
@@ -1,4 +1,5 @@
 "use server";
+import { revalidatePath } from "next/cache";
 import prisma from "../prisma/prisma";
 
 export async function FetchMessages(postId: string) {
@@ -21,4 +22,17 @@ export async function FetchMessages(postId: string) {
   return messages;
 }
 
-export async function CreateMessage(postId: string, content: string) {}
+export async function CreateMessage(formdata: FormData) {
+  const postId = formdata.get("postId");
+  const userId = formdata.get("userId");
+  const content = formdata.get("content");
+
+  const message = await prisma.message.create({
+    data: {
+      postId: postId as string,
+      userId: userId as string,
+      content: content as string,
+    },
+  });
+  revalidatePath("/post/[id]");
+}

6cd46ff9d96d74ef04c42799b5e37e487edd6eda
Nick <darkforceyoda@gmail.com>
2024-01-04 09:28:45 -0600

    signup form

--- components/forms/SignUpForm.tsx
+++ components/forms/SignUpForm.tsx
@@ -1,14 +1,46 @@
+import { CreateUser } from "@/lib/actions/user.actions";
 import React from "react";
 
 type SignUpFormProps = {};
 
-const SignUpForm: React.FC<SignUpFormProps> = () => {
+const SignUpForm: React.FC<SignUpFormProps> = async () => {
   return (
     <form
       className="space-y-6 bg-[#0d1117] rounded-3xl text-white px-4 py-8"
-      action="#"
+      action={CreateUser}
       method="POST"
     >
+      <div>
+        <label htmlFor="name" className="block text-sm font-medium leading-6">
+          Name:
+        </label>
+        <div className="mt-2">
+          <input
+            id="name"
+            name="name"
+            type="text"
+            required
+            className="block bg-[#0d1117] border border-gray-600 rounded-lg w-full rounded-m py-1.5 shadow-sm placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 px-4"
+          />
+        </div>
+      </div>
+      <div>
+        <label
+          htmlFor="username"
+          className="block text-sm font-medium leading-6"
+        >
+          Username:
+        </label>
+        <div className="mt-2">
+          <input
+            id="username"
+            name="username"
+            type="text"
+            required
+            className="block bg-[#0d1117] border border-gray-600 rounded-lg w-full rounded-m py-1.5 shadow-sm placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 px-4"
+          />
+        </div>
+      </div>
       <div>
         <label htmlFor="email" className="block text-sm font-medium leading-6">
           Email address:
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,12 +1,28 @@
 "use server";
+import { revalidatePath } from "next/cache";
 import prisma from "../prisma/prisma";
+import { redirect } from "next/navigation";
 
 type User = { username: string; name: string; password: string; email: string };
 
-export async function CreateUser({ name, username, email, password }: User) {
+export async function CreateUser(
+  // { name, username, email, password }: User
+  formData: FormData
+) {
+  const username = formData.get("username");
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const password = formData.get("password");
+
   const user = await prisma.user.create({
-    data: { username, name, email, password },
+    data: {
+      username: username as string,
+      name: name as string,
+      email: email as string,
+      password: password as string,
+    },
   });
+  redirect("/sign-in");
 }
 
 export async function fetchUserByEmail(email: string) {
